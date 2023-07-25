0e1a68572f8e93a9036accfcbe7a85c70326a3a3
mnmonherdene1234 <mnmonherdene1234@gmail.com>
2023-07-25 20:52:31 +0800

    first

---
+++ Controllers/UploadController.cs
@@ -0,0 +1,20 @@
+﻿using BunnyUpload.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BunnyUpload.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UploadController : ControllerBase
+    {
+        [HttpPost]
+        public UploadResult Upload()
+        {
+            return new UploadResult
+            {
+                Url = "Hello World!"
+            };
+        }
+    }
+}
---
+++ Models/UploadResult.cs
@@ -0,0 +1,7 @@
+﻿namespace BunnyUpload.Models
+{
+    public class UploadResult
+    {
+        public string Url { get; set; }
+    }
+}
